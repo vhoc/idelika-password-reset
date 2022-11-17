b8369680a63340d53747dc37f881c22cd99c94e9
Victor Olvera <vhocar@gmail.com>
2022-11-17 10:43:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
+import FormSchema from './FormSchema';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+      <Formik
+        initialValues={{
+          password: '',
+          passwordconfirmation: '',
+        }}
+        validationSchema={FormSchema}
+        onSubmit={values => {
+          console.log(values)
+        }}
+      >
+
+        {({ errors, touched }) => (
+          <Form>
+
+            <Field name="password" />
+            {errors.password && touched.password ? (
+              <div>{errors.password}</div>
+            ) : null}
+
+            <Field name="passwordConfirmation" />
+            {errors.passwordConfirmation && touched.passwordConfirmation ? (
+              <div>{errors.passwordConfirmation}</div>
+            ) : null}
+
+          </Form>
+        )}
+
+      </Formik>
+
     </div>
   );
 }
---
+++ src/FormSchema.js
@@ -0,0 +1,51 @@
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
+
+const FormSchema = Yup.object().shape({
+    password: Yup.string()
+      .min(8, 'La contraseña debe tener mínimo 8 caracteres.')
+      .max(100, "La contraeña no puede exceder 100 caracteres.")
+      .test('isValidPass', ' no es válida', (value, context) => {
+        const hasUpperCase = /[A-Z]/.test(value)
+          const hasLowerCase = /[a-z]/.test(value)
+          const hasNumber = /[0-9]/.test(value)
+          const hasSymbole = /[!@#%&]/.test(value)
+          let validConditions = 0
+          const numberOfMustBeValidConditions = 3
+          const conditions = [hasLowerCase, hasUpperCase, hasNumber, hasSymbole]
+          conditions.forEach((condition) =>
+            condition ? validConditions++ : null
+          )
+          if (validConditions >= numberOfMustBeValidConditions) {
+            return true;
+          }
+          return false;
+  
+      })
+      .required('Se requiere especificar una contraseña.'),
+  
+      passwordConfirmation: Yup.string()
+        .min(8, 'La contraseña debe tener mínimo 8 caracteres.')
+        .max(100, "La contraeña no puede exceder 100 caracteres.")
+        .test('isValidPass', ' no es válida', (value, context) => {
+          const hasUpperCase = /[A-Z]/.test(value)
+            const hasLowerCase = /[a-z]/.test(value)
+            const hasNumber = /[0-9]/.test(value)
+            const hasSymbole = /[!@#%&]/.test(value)
+            let validConditions = 0
+            const numberOfMustBeValidConditions = 3
+            const conditions = [hasLowerCase, hasUpperCase, hasNumber, hasSymbole]
+            conditions.forEach((condition) =>
+              condition ? validConditions++ : null
+            )
+            if (validConditions >= numberOfMustBeValidConditions) {
+              return true;
+            }
+            return false;
+  
+        })
+        .required('Se requiere especificar una contraseña.')
+        .oneOf([Yup.ref('password'), null], 'Las contraseñas deben coincidir.'),
+  })
+
+  export default FormSchema
